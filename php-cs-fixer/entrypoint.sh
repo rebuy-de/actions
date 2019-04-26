3a32db7b16a7422cb5b529bb7a793b34f1d8142d
Martin Parsiegla <mp@rebuy.com>
2019-04-26 18:57:41 +0200

    Add config path

--- php-cs-fixer/entrypoint.sh
+++ php-cs-fixer/entrypoint.sh
@@ -6,11 +6,13 @@ set -euo pipefail
 source /lib.sh
 
 lint() {
-	_git_changed_files | php-cs-fixer fix --dry-run -v --diff --path-mode=intersection
+	_git_changed_files | xargs php-cs-fixer fix --config=.php_cs.dist --dry-run -v --diff --path-mode=intersection
 }
 
 fix() {
-	_git_changed_files | php-cs-fixer fix -v --path-mode=intersection
+	_git_changed_files | xargs php-cs-fixer fix --config=.php_cs.dist -v --path-mode=intersection
 }
+
+echo $GITHUB_EVENT_NAME
 
 _lint_and_fix_action php-cs-fixer "${@}"
