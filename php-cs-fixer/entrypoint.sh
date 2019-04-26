7e30c0066cef7446f68758f58930e518a9b6cf9e
Martin Parsiegla <mp@rebuy.com>
2019-04-26 18:24:18 +0200

    Only fix changed files

--- php-cs-fixer/entrypoint.sh
+++ php-cs-fixer/entrypoint.sh
@@ -6,11 +6,11 @@ set -euo pipefail
 source /lib.sh
 
 lint() {
-	php-cs-fixer fix --dry-run -v --diff
+	_git_changed_files | php-cs-fixer fix --dry-run -v --diff --path-mode=intersection
 }
 
 fix() {
-	php-cs-fixer fix
+	_git_changed_files | php-cs-fixer fix -v --path-mode=intersection
 }
 
 _lint_and_fix_action php-cs-fixer "${@}"
